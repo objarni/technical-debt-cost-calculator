26b7f742e3de736e0c8fa4676a00fa25b398e347
Olof Bjarnason <olof.bjarnason@gmail.com>
2021-03-12 19:56:25 +0100

    add non interactive slider for wage

--- src/Main.elm
+++ src/Main.elm
@@ -1,24 +1,30 @@
 module Main exposing (..)
 
 import Element exposing (Element)
-import Element.Background
+import Element.Background as Background
+import Element.Border as Border
+import Element.Input as Input
 import Html exposing (Html)
 
+type Model = Model Int
 
-main : Html msg
+type Msg = UpdateWage Float
+
+
+main : Html Msg
 main =
     Element.layout []
         calculator
 
 
-calculator : Element a
+calculator : Element Msg
 calculator =
     Element.column
         [ Element.centerX
         , Element.centerY
         , Element.padding 40
         , Element.spacing 40
-        , Element.Background.color (Element.rgb255 230 230 250)
+        , Background.color (Element.rgb255 230 230 250)
         ]
         [ title, box, approximation ]
 
@@ -28,11 +34,15 @@ title =
 
 
 box =
+    --let
+    --    avgWage =
+    --        Element.text "Snittlön (kr)"
+    --in
     Element.column
         -- these two centers the Element.text in div
         [ Element.centerX, Element.centerY ]
         [ Element.text "Antal utvecklare"
-        , Element.text "Snittlön (kr)"
+        , avgWage
         , Element.text "Uppskattad fördelning buggar/omskrivning (%)*"
         , Element.text "Uppskattad kostnad teknisk skuld (per månad)"
         ]
@@ -40,3 +50,31 @@ box =
 
 approximation =
     Element.text "Mycket pengar"
+
+avgWage =
+    Input.slider
+        [ Element.height (Element.px 30)
+
+        -- Here is where we're creating/styling the "track"
+        , Element.behindContent
+            (Element.el
+                [ Element.width Element.fill
+                , Element.height (Element.px 2)
+                , Element.centerY
+                , Background.color (Element.rgb255  0 0 250)
+                , Border.rounded 2
+                ]
+                Element.none
+            )
+        ]
+        { onChange = UpdateWage
+        , label =
+            Input.labelAbove []
+                (Element.text "Genomsnittslön (per månad)")
+        , min = 20000
+        , max = 75000
+        , step = Just 2500
+        , value = 25000
+        , thumb =
+            Input.defaultThumb
+        }
